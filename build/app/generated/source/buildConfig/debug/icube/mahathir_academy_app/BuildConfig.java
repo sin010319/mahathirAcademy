414734e94bf46be008cbefed40b11a225b8de6ca
LAMXINLE <xlam0003@student.monash.edu>
2021-04-11 19:48:49 +0800

    pull

---
+++ build/app/generated/source/buildConfig/debug/icube/mahathir_academy_app/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package icube.mahathir_academy_app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "icube.mahathir_academy_app";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
